d07c3d29ecd15a39133541febca154bda1eb8fa1
Prometheumtbc <79119688+Prometheumtbc@users.noreply.github.com>
2023-12-20 23:41:46 +1300

    created moonwave documentation

--- src/init.lua
+++ src/init.lua
@@ -1,23 +1,49 @@
---[[
-    attune
-    a tiny utility module for pitching sounds with 12-TET
+--[=[
+    @class Attune
+    a tiny utility module for pitching sounds with 12-TET.
+]=]--
 
-    by Primithium
-]]--
+--[=[
+    @prop Effect PitchShiftSoundEffect
+    @within Attune
+    @readonly
+    a reference to the [PitchShiftSoundEffect](https://create.roblox.com/docs/reference/engine/classes/PitchShiftSoundEffect) linked to this Attune object.
+    for almost all use cases this should not be modified, however, its properties can be.
+]=]--
+
+--[=[
+    @prop Tuning number
+    @within Attune
+    the offset (in cents) to apply to the sound before any operation. intended to be used for fine tuning samples.
+]=]--
 
 local Attune = {}
 Attune.__index = Attune
 
-function Attune.Init(Sound)
+--[=[
+    creates a new Attune object and instantiates a [PitchShiftSoundEffect](https://create.roblox.com/docs/reference/engine/classes/PitchShiftSoundEffect) for future use.
+
+    @param Sound Sound -- the [Sound](https://create.roblox.com/docs/reference/engine/classes/Sound) you want to tune.
+    @param Tuning number -- (optional) the offset (in cents) to apply to the sound before any operation. intended to be used for fine tuning samples.
+    @return Attune
+]=]--
+
+function Attune.Init(Sound, Tuning)
     if not Sound:IsA("Sound") then warn("Attempted to initialize Attune on non-sound object") return end
 
     local object = {}
     object.Effect = Instance.new("PitchShiftSoundEffect", Sound)
     object.Effect.Octave = 1
-    object.Tuning = 0
+    object.Tuning = Tuning or 0
     setmetatable(object, Attune)
     return object
 end
+
+--[=[
+    offsets the pitch of the sample by a number of semitones.
+
+    @param Offset number -- the number (in semitones) to adjust by.
+]=]--
 
 function Attune:SetShiftInSemitones(Offset)
     local pitchFactor = 2 ^ ((Offset + self.Tuning) / 12)
@@ -25,6 +51,12 @@ function Attune:SetShiftInSemitones(Offset)
     
     self.Effect.Octave = pitchFactor
 end
+
+--[=[
+    offsets the pitch of the sample by a number of cents.
+
+    @param Offset number -- the number (in cents) to adjust by.
+]=]--
 
 function Attune:SetShiftInCents(Offset)
     local pitchFactor = 2 ^ ((Offset + self.Tuning) / 1200)
